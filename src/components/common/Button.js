5de12964a317b9f84d399733b728c55196190065
inns2110 <inns2110@gmail.com>
2025-02-21 17:00:08 +0900

    Design Button

---
+++ src/components/common/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = ({ onClick, children, style }) => (
+  <StyledButton onClick={onClick} style={style}>
+    {children}
+  </StyledButton>
+);
+
+const StyledButton = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: #E5E7EB; 
+  border-radius: 10px;
+  height: 40px;
+`;
+
+export default Button;
